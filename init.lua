a3796c311498bcbb2b058db4f873f0c4a4a519d1
jmario74 <84346469+jmario74@users.noreply.github.com>
2023-10-24 13:30:51 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -284,3 +284,45 @@ local function mrk()
 	buffer.add_text(' ⌘')
 end
 keys['ctrl+kp3'] = mrk
+
+-- thank you to Eric Anderson for his show & tell of this code
+-- show opening block in status bar, closing block must not have space before it
+function string.trim(str)
+  return str:gsub('^%s+', ''):gsub('%s+$', '')
+end
+
+events.connect(events.UPDATE_UI, function(updated)
+  -- We only care when the cursor moves
+  if not (updated & buffer.UPDATE_SELECTION) then return end
+
+  local line = buffer:line_from_position(buffer.current_pos)
+  local line_text = buffer.get_line(line)
+  local blank = line_text == "\n"
+
+  -- Starting line is blank so nothing to match up against
+  if blank then return end
+
+  -- First line can't match up to anything so return early otherwise we get an
+  -- error trying to read the previous line.
+  if line <= 1 then return end
+
+  local cur_indent = buffer.line_indentation[line]
+  local prev_indent = buffer.line_indentation[line-1]
+
+  -- If the previous line is not nested, return early as we are not ending a indention match
+  if prev_indent <= cur_indent then return end
+
+  repeat
+    line = line - 1
+    prev_indent = buffer.line_indentation[line]
+    line_text = buffer:get_line(line)
+    blank = line_text == "\n"
+  until prev_indent <= cur_indent and not blank
+
+  line_text = line_text:trim()
+
+  if line_text == '/*' then return end
+  if line_text == '/**' then return end
+
+  ui.statusbar_text = buffer.filename .. "	" .. "block start: " .. line_text
+end)
